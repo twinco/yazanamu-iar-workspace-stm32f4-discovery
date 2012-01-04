252a093101c47b6830694d7f7209c7028a67fb64
yazanamu <yazanamu@8df6e120-7bdc-5c29-f44a-06b08f1c46b6>
2012-01-04 15:56:10 +0000

    Updated.

--- test02/main.c
+++ test02/main.c
@@ -10,7 +10,7 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f4_discovery.h"
-
+#include "stdio.h"
 
 /** @addtogroup STM32F4_Discovery_Peripheral_Examples
   * @{
@@ -35,6 +35,78 @@ void Delay(__IO uint32_t nCount);
   * @param  None
   * @retval None
   */
+
+void delay_1_second(void)
+{
+    Delay(1000);
+}
+
+void delay_100_milli_second(void)
+{
+    Delay(100);
+}
+
+void USART1_Init(void)
+{
+    USART_InitTypeDef USART_InitStructure;
+
+    /* USARTx configuration ------------------------------------------------------*/
+    /* USARTx configured as follow:
+     - BaudRate = 115200 baud  
+     - Word Length = 8 Bits
+     - One Stop Bit
+     - No parity
+     - Hardware flow control disabled (RTS and CTS signals)
+     - Receive and transmit enabled
+     */
+    USART_InitStructure.USART_BaudRate   = 115200;
+    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+    USART_InitStructure.USART_StopBits   = USART_StopBits_1;
+    USART_InitStructure.USART_Parity     = USART_Parity_No ;
+    USART_InitStructure.USART_HardwareFlowControl
+                                         = USART_HardwareFlowControl_None;
+    USART_InitStructure.USART_Mode       = USART_Mode_Rx | USART_Mode_Tx;
+
+    /* Configure the USARTx */ 
+    USART_Init(USART1, &USART_InitStructure);
+
+    /* Enable the USART1 */
+    USART_Cmd(USART1, ENABLE);
+}
+
+/*
+ * Name   : RCC_Configuration
+ * Input  : None
+ * Output : None
+ * Return : None
+ */
+void RCC_Configuration(void)
+{
+    SystemInit();
+
+    /* Enable GPIOA clock */
+//    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+
+    /* Enable GPIOB clock */
+//    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+
+    /* Enable GPIOC clock */
+//    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+
+    /* Enable GPIOD clock */
+//    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
+
+    /* Enable USART1 clocks */
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+
+    /* Enable AFIO clock */
+//    RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+
+    /* Enable DMA1 clock */
+//    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
+}
+
+
 int main(void)
 {
   /* GPIOD Periph clock enable */
@@ -48,10 +120,11 @@ int main(void)
   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
   GPIO_Init(GPIOD, &GPIO_InitStructure);
 
-  RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
-//  RCC_AHB1PeriphClockCmd();
-//  USART_Init();
-  USART_Cmd(USART2, ENABLE);
+  /* Configigure UAER2 */
+
+  USART1_Init();
+  
+  RCC_Configuration();
   
   
   while (1)
@@ -84,6 +157,9 @@ int main(void)
     
     /* Insert delay */
     Delay(0xFFFFFF);
+	
+  	printf("Hello World\r\n");
+	
   }
 }
 
