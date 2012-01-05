87dc1a219aecaf944be4df0d60583b154d108abc
yazanamu <yazanamu@8df6e120-7bdc-5c29-f44a-06b08f1c46b6>
2012-01-05 15:34:31 +0000

    Updated. TEST2 with USART

--- test02/main.c
+++ test02/main.c
@@ -10,7 +10,9 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f4_discovery.h"
-#include "stdio.h"
+#include "stm32f4xx.h"
+#include "main.h"
+#include <stdio.h>
 
 /** @addtogroup STM32F4_Discovery_Peripheral_Examples
   * @{
@@ -21,20 +23,49 @@
   */ 
 
 /* Private typedef -----------------------------------------------------------*/
-GPIO_InitTypeDef  GPIO_InitStructure;
+//GPIO_InitTypeDef  GPIO_InitStructure;
+USART_InitTypeDef USART_InitStructure;
+USART_TypeDef* COM_USART[COMn] = {EVAL_COM1}; 
+GPIO_TypeDef* COM_TX_PORT[COMn] = {EVAL_COM1_TX_GPIO_PORT};
+GPIO_TypeDef* COM_RX_PORT[COMn] = {EVAL_COM1_RX_GPIO_PORT};
 
 /* Private define ------------------------------------------------------------*/
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
+const uint32_t COM_USART_CLK[COMn] = {EVAL_COM1_CLK};
+const uint32_t COM_TX_PORT_CLK[COMn] = {EVAL_COM1_TX_GPIO_CLK};
+const uint32_t COM_RX_PORT_CLK[COMn] = {EVAL_COM1_RX_GPIO_CLK};
+const uint16_t COM_TX_PIN[COMn] = {EVAL_COM1_TX_PIN};
+const uint16_t COM_RX_PIN[COMn] = {EVAL_COM1_RX_PIN};
+const uint16_t COM_TX_PIN_SOURCE[COMn] = {EVAL_COM1_TX_SOURCE};
+const uint16_t COM_RX_PIN_SOURCE[COMn] = {EVAL_COM1_RX_SOURCE};
+const uint16_t COM_TX_AF[COMn] = {EVAL_COM1_TX_AF};
+const uint16_t COM_RX_AF[COMn] = {EVAL_COM1_RX_AF};
+
 /* Private function prototypes -----------------------------------------------*/
 void Delay(__IO uint32_t nCount);
+#ifdef __GNUC__
+  /* With GCC/RAISONANCE, small printf (option LD Linker->Libraries->Small printf
+     set to 'Yes') calls __io_putchar() */
+  #define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+  #define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif /* __GNUC__ */
+
 /* Private functions ---------------------------------------------------------*/
 
 /**
-  * @brief  Main program
-  * @param  None
+  * @brief  Delay Function.
+  * @param  nCount:specifies the Delay time length.
   * @retval None
   */
+void Delay(__IO uint32_t nCount)
+{
+  while(nCount--)
+  {
+  }
+}
+
 
 void delay_1_second(void)
 {
@@ -46,32 +77,35 @@ void delay_100_milli_second(void)
     Delay(100);
 }
 
-void USART1_Init(void)
-{
-    USART_InitTypeDef USART_InitStructure;
-
-    /* USARTx configuration ------------------------------------------------------*/
-    /* USARTx configured as follow:
-     - BaudRate = 115200 baud  
-     - Word Length = 8 Bits
-     - One Stop Bit
-     - No parity
-     - Hardware flow control disabled (RTS and CTS signals)
-     - Receive and transmit enabled
-     */
-    USART_InitStructure.USART_BaudRate   = 115200;
-    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
-    USART_InitStructure.USART_StopBits   = USART_StopBits_1;
-    USART_InitStructure.USART_Parity     = USART_Parity_No ;
-    USART_InitStructure.USART_HardwareFlowControl
-                                         = USART_HardwareFlowControl_None;
-    USART_InitStructure.USART_Mode       = USART_Mode_Rx | USART_Mode_Tx;
-
-    /* Configure the USARTx */ 
-    USART_Init(USART1, &USART_InitStructure);
-
-    /* Enable the USART1 */
-    USART_Cmd(USART1, ENABLE);
+/*
+ * Name   : GPIO_Configuration
+ * Input  : None
+ * Output : None
+ * Return : None
+ */
+void GPIO_Configuration(void)
+{  
+	/*!< At this stage the microcontroller clock setting is already configured, 
+		 this is done through SystemInit() function which is called from startup
+		 file (startup_stm32f4xx.s) before to branch to application main.
+		 To reconfigure the default setting of SystemInit() function, refer to
+		 system_stm32f4xx.c file
+	   */	  
+		 
+	/* USARTx configured as follow:
+		  - BaudRate = 115200 baud	
+		  - Word Length = 8 Bits
+		  - One Stop Bit
+		  - No parity
+		  - Hardware flow control disabled (RTS and CTS signals)
+		  - Receive and transmit enabled
+	*/
+	USART_InitStructure.USART_BaudRate = 115200;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
 }
 
 /*
@@ -82,7 +116,10 @@ void USART1_Init(void)
  */
 void RCC_Configuration(void)
 {
-    SystemInit();
+//    SystemInit();
+
+	/* GPIOD Periph clock enable */
+//	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 
     /* Enable GPIOA clock */
 //    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
@@ -97,7 +134,7 @@ void RCC_Configuration(void)
 //    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
 
     /* Enable USART1 clocks */
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+//    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
 
     /* Enable AFIO clock */
 //    RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
@@ -106,27 +143,79 @@ void RCC_Configuration(void)
 //    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
 }
 
+/**
+  * @brief  Configures COM port.
+  * @param  COM: Specifies the COM port to be configured.
+  *   This parameter can be one of following parameters:    
+  *     @arg COM1
+  *     @arg COM2  
+  * @param  USART_InitStruct: pointer to a USART_InitTypeDef structure that
+  *   contains the configuration information for the specified USART peripheral.
+  * @retval None
+  */
+void STM_EVAL_COMInit(COM_TypeDef COM, USART_InitTypeDef* USART_InitStruct)
+{
+  GPIO_InitTypeDef GPIO_InitStructure;
 
+  /* Enable GPIO clock */
+  RCC_AHB1PeriphClockCmd(COM_TX_PORT_CLK[COM] | COM_RX_PORT_CLK[COM], ENABLE);
+
+  if (COM == COM1)
+  {
+    /* Enable UART clock */
+    RCC_APB1PeriphClockCmd(COM_USART_CLK[COM], ENABLE);
+  }
+
+  /* Connect PXx to USARTx_Tx*/
+  GPIO_PinAFConfig(COM_TX_PORT[COM], COM_TX_PIN_SOURCE[COM], COM_TX_AF[COM]);
+
+  /* Connect PXx to USARTx_Rx*/
+  GPIO_PinAFConfig(COM_RX_PORT[COM], COM_RX_PIN_SOURCE[COM], COM_RX_AF[COM]);
+
+  /* Configure USART Tx as alternate function  */
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+
+  GPIO_InitStructure.GPIO_Pin = COM_TX_PIN[COM];
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(COM_TX_PORT[COM], &GPIO_InitStructure);
+
+  /* Configure USART Rx as alternate function  */
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_Pin = COM_RX_PIN[COM];
+  GPIO_Init(COM_RX_PORT[COM], &GPIO_InitStructure);
+
+  /* USART configuration */
+  USART_Init(COM_USART[COM], USART_InitStruct);
+    
+  /* Enable USART */
+  USART_Cmd(COM_USART[COM], ENABLE);
+}
+
+
+/**
+  * @brief  Main program
+  * @param  None
+  * @retval None
+  */
 int main(void)
 {
-  /* GPIOD Periph clock enable */
-  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+#ifdef DEBUG
+	debug();
+#endif
 
-  /* Configure PD12, PD13, PD14 and PD15 in output pushpull mode */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-  GPIO_Init(GPIOD, &GPIO_InitStructure);
 
   /* Configigure UAER2 */
-
-  USART1_Init();
-  
+  /* System clocks configuration ---------------------------------------------*/
   RCC_Configuration();
-  
-  
+  GPIO_Configuration();
+  //NVIC_Configuration();  
+  STM_EVAL_COMInit(COM1, &USART_InitStructure);
+
+  /* Output a message on Hyperterminal using printf function */
+  printf("\n\rUSART Printf Example: retarget the C library printf function to the USART\n\r");
+
   while (1)
   {
     /* PD12 to be toggled */
@@ -158,22 +247,27 @@ int main(void)
     /* Insert delay */
     Delay(0xFFFFFF);
 	
-  	printf("Hello World\r\n");
-	
   }
 }
 
 /**
-  * @brief  Delay Function.
-  * @param  nCount:specifies the Delay time length.
+  * @brief  Retargets the C library printf function to the USART.
+  * @param  None
   * @retval None
   */
-void Delay(__IO uint32_t nCount)
+PUTCHAR_PROTOTYPE
 {
-  while(nCount--)
-  {
-  }
+  /* Place your implementation of fputc here */
+  /* e.g. write a character to the USART */
+  USART_SendData(EVAL_COM1, (uint8_t) ch);
+
+  /* Loop until the end of transmission */
+  while (USART_GetFlagStatus(EVAL_COM1, USART_FLAG_TC) == RESET)
+  {}
+
+  return ch;
 }
+
 
 #ifdef  USE_FULL_ASSERT
 
